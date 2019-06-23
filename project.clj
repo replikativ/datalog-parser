dc8ee965fda3124f3e972cf5bcbd6a9a9d6a405e
Konrad Kühne <konrad.kuehne@lambdaforge.io>
2019-06-23 16:08:21 +0200

    set version to 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject io.lambdaforge/datalog-parser "0.0.1"
+(defproject io.lambdaforge/datalog-parser "0.1.0"
   :description  "Datalog parser."
   :url          "https://github.com/lambdaforge/datalog-parser"
   :license      {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
