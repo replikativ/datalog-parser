b734b7ea9d7085b42af99855adf85635acb9418e
bsless <ben.sless@gmail.com>
2020-06-05 19:11:19 +0300

    Add performance test for parser

---
+++ perf/datalog/parser_perf.clj
@@ -0,0 +1,30 @@
+(ns datalog.parser-perf
+  (:require [clojure.test :refer [deftest]]
+            [datalog.parser :as parser]
+            [datalog.parser.test.util]
+            [criterium.core :as cc]
+            [clj-async-profiler.core :as prof]))
+
+(def q '[:find ?e
+         :in $ ?fname ?lname
+         :keys foo
+         :where [?e :user/firstName ?fname]
+         [?e :user/lastName ?lname]])
+
+(deftest parse
+  (cc/bench (parser/parse q)))
+
+(comment
+  (prof/serve-files 7777))
+
+(def reps 1e6)
+
+(deftest profile
+  (prof/profile
+   (time
+    (dotimes [_ reps]
+      (parser/parse q)))))
+
+
+
+
