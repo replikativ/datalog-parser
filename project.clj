a783f49411a929bb79d9e9e3de7a387642d050ab
bsless <ben.sless@gmail.com>
2020-06-05 19:10:35 +0300

    Add profiling utilities to dev profile

--- project.clj
+++ project.clj
@@ -6,4 +6,7 @@
   :dependencies [[org.clojure/clojure "1.9.0"]]
   :repositories [["clojars" {:url "https://clojars.org/repo"
                              :sign-releases false}]]
+  :profiles {:dev {:source-paths ["perf"]
+                   :dependencies [[com.clojure-goes-fast/clj-async-profiler "0.4.1"]
+                                  [criterium "0.4.5"]]}}
   :repl-options {:init-ns datalog.parser})
